fb15053ccb58b50f12b74fbe0262e1bf049e8b24
jiwon <jjw6477@gmail.com>
2019-06-05 01:02:27 +0900

    06.05 Commit(include RecyclerView)

--- app/src/main/java/com/example/capstone01/Fragment/SocialFragment.java
+++ app/src/main/java/com/example/capstone01/Fragment/SocialFragment.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.capstone01.Adapter.SampleData;
+import com.example.capstone01.Adapter.SocialAdapter;
 import com.example.capstone01.R;
 
 public class SocialFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private SocialAdapter adapter = new SocialAdapter();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_social, container, false);
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_social,container, false);
+
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setItems(new SampleData().getItems());
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/example/capstone01/item/Social.java
@@ -0,0 +1,22 @@
+package com.example.capstone01.item;
+
+public class Social {
+
+
+
+    private String url;
+    private String title;
+
+    public Social(String url, String title){
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
